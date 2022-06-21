35afe121fd901b0bca5de4cfee0f1981795fc58a
theme <theme.2541@gmail.com>
2022-06-21 21:15:46 +0700

    merged controllers

--- Routes/authRoute.js
+++ Routes/authRoute.js
@@ -3,15 +3,15 @@ const express = require("express");
 const authRoute = express.Router();
 
 const authController = require("../controllers/authController");
-const authCustomerController = require("../controllers/authCustomerController");
+
 
 // RESTAURANT
-authRoute.post("/login/restaurant");
+authRoute.post("/login/restaurant", authController.loginRestaurant);
 
 authRoute.post("/register/restaurant", authController.registerRestaurant);
 
 // CUSTOMER
-authRoute.post("/register/customer", authCustomerController.signup);
-authRoute.post("/register/customer", authCustomerController.login);
+authRoute.post("/register/customer", authController.registerCustomer);
+authRoute.post("/login/customer", authController.loginCustomer);
 
 module.exports = authRoute;
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,28 +1,116 @@
-const {Restaurant} = require('../models');
+const {Restaurant, Customer} = require('../models');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const createError = require('../services/createError');
-require('dotenv').config()
+require('dotenv').config();
+
+const { Op } = require('sequelize')
+
+console.log(process.env.JWT_EXPIRES_IN)
+const genToken = (payload) =>
+  jwt.sign(payload, process.env.JWT_SECRET_KEY, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
 
 //Only for register with email/password
 exports.registerRestaurant = async (req, res, next) => {
     try {
 
         
-        const {name, latitude, longitude, email, password, telephoneNumber } = req.body;
+        const {name, latitude, longitude, email, password, telephoneNumber, confirmPassword } = req.body;
 
-        const token = jwt.sign({email, role: "restaurant"}, process.env.JWT_SECRET_KEY, {
-            expiresIn: process.env.JWT_EXPIRES
-        });
+        if (password !== confirmPassword) createError("invalid credentials", 400);
+
         
         const hashedPw = await bcrypt.hash(password,10);
-    
+        
+        const token =  genToken({email});
+
         await Restaurant.create({name, latitude, longitude, email, password: hashedPw, telephoneNumber})
         
-        res.json({token})
+        res.status(201).json({ message: "sign up success" ,token})
     
     } catch (err) {
         next(err)
     }
 }
 
+exports.loginRestaurant = async (req, res, next) => {
+    try {
+        const {email, password} = req.body;
+        
+        const restaurant = await Restaurant.findOne({where : {
+            email
+        }});
+
+        const isCorrect = await bcrypt.compare( password, restaurant.password);
+
+        if (!isCorrect) createError("invalid email or password", 400);
+
+        const token = genToken({email});
+
+        res.json({message: "Login success", token})
+
+    } catch (err) {
+        next(err)
+    }
+}
+
+
+exports.registerCustomer = async (req, res, next) => {
+    try {
+      const { firstName, lastName, email, gmail, password, confirmPassword } =
+        req.body;
+  
+      if (!email && gmail) {
+      }
+  
+      if (!password) {
+      }
+  
+      if (password !== confirmPassword) {
+      }
+  
+      const hashedPassword = await bcrypt.hash(password, 10);
+  
+      const customer = await Customer.create({
+        firstName,
+        lastName,
+        email,
+        gmail,
+        password: gmail ? null : hashedPassword,
+      });
+  
+      const token = genToken({ email });
+  
+      res.status(201).json({ message: "Sign up success", token });
+    } catch (err) {
+      next(err);
+    }
+  };
+  
+  exports.loginCustomer = async (req, res, next) => {
+    try {
+      const { emailOrGmail, password } = req.body;
+  
+      const customer = await Customer.findOne({
+        where: {
+          [Op.or]: [{ email: emailOrGmail }, { gmail: emailOrGmail }],
+        },
+      });
+  
+      if (!customer) {
+      }
+  
+      const isMatch = await bcrypt.compare(password, customer.password);
+  
+      if (!isMatch) {
+      }
+  
+      const token = genToken({ email: emailOrGmail });
+      res.json({ message: "Login success", token });
+    } catch (err) {
+      next(err);
+    }
+  };
+  
