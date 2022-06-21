df0da9a97dce159ca55698718f8e20da7ad6cb24
Rose <rozurosemary@gmail.com>
2022-06-21 17:54:08 +0700

    create authCustomer route

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
-const express = require('express');
+const express = require("express");
 require("dotenv").config();
-const {sequelize} = require('./models');
-
-
+const { sequelize } = require("./models");
 
 const PORT = process.env.PORT || 5000;
 
-
 const app = express();
 
-app.listen(PORT,() => {
-    console.log("listening on http://localhost:"+PORT);
-    sequelize.sync({alter: true});
-})                  
+app.use("/");
+
+app.listen(PORT, () => {
+  console.log("listening on http://localhost:" + PORT);
+  sequelize.sync({ alter: true });
+});
