3e06d22d4d4a97fc4059f0a6dd5a510ae7cb1d0b
pkpukaow <pukaowtee@gmail.com>
2022-07-03 22:29:58 +0700

    foodmap

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, path.join(__dirname, 'public/images'));
+    cb(null, path.join(__dirname, '../public/images'));
   },
   filename: (req, file, cb) => {
     cb(null, uuidv4() + '.' + file.mimetype.split('/')[1]);
